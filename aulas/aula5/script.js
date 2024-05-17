72b3a93b239d30aa4c9acc76f5183fa7c6d10d73
LuizHBB <luiz.belorio@usp.br>
2024-05-17 13:24:27 -0300

    Aula 5 - JavaScript 1, exercico 2

--- aulas/aula5/script.js
+++ aulas/aula5/script.js
@@ -1,3 +1,11 @@
 function muda_cor(){
     document.querySelector('p').style.color = 'red'
 }
+
+function mudaAzul(){
+    document.querySelector('.area-quadrada').style.border = '5px solid blue'
+}
+
+function mudaVermelho(){
+    document.querySelector('.area-quadrada').style.border = '5px solid red'
+}
