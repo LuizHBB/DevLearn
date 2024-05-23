b7adac32bd01e5a4fed44d4c2beab1a141c7fc09
LuizHBB <luiz.belorio@usp.br>
2024-05-22 22:23:43 -0300

    aula 7 - JavaScript 3, exercicio 2

--- aulas/aula7/script.js
+++ aulas/aula7/script.js
@@ -10,3 +10,18 @@ function calcVolume(){
     jsVolume = 3.14*jsRaio*jsRaio*jsAltura;
     document.getElementById('volume').textContent = jsVolume;
 }
+
+function trocaCor(){
+    const jsCor = document.getElementById('cor');
+    const jsChangeColor = document.getElementById('changeColor');
+    const cor = jsCor.value; // Pegar o valor do input
+
+    // Atualizar o texto do span
+    jsChangeColor.textContent = cor;
+
+    // Alterar a cor do texto na div
+    document.querySelector('.container-ex2').style.color = cor;
+
+    return false; // Prevenir o comportamento padrão do formulário
+}
+trocaCor();
